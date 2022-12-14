88d456343e9ce2f0b9a9b7075086183f1e474f12
f.cinelli@bizcode.it <f.cinelli@bizcode.it>
2022-12-15 00:24:55 +0100

    Add Funcion

--- marsrover.js
+++ marsrover.js
@@ -1,38 +1,67 @@
 
-const mars = [10, 10];
+
+;
+class Planet {
+    constructor(squaredGridDimension = 10) {
+        this.maxX = squaredGridDimension - 1;
+        this.maxY = squaredGridDimension -1 ;
+    }
+
+}
 
 
+class Rover {
+    constructor(x = 0, y = 0, direction = "N") {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;  //N S W E
+    }
 
+    move(command){
+        console.log(`moving ${command}`)
+        if (command == "f"){
+            this.y = this.y + 1;
+        }
 
+        if (command == "b"){
+            this.y = this.y - 1;
+        }
+    }
 
+    turn() {
 
+    }
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Obstacle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 const start = () => {
-    console.log("ciao");
+   const mars = new Planet;
+   const rover = new Rover;
+   
+   rover.move("b")
+   whereIsMyRover(rover)
+   rover.move("f")
+   whereIsMyRover(rover)
+}
+
+const whereIsMyRover  = (rover) => {
+    console.log(`Now the Rover is X:${rover.x}, Y: ${rover.y}, facing: ${rover.direction}`);
 }
 
 start();
+
+
+
+/*
+
+1,1 N
+1, 2 N
+
+*/
