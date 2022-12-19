a3edd0e54c443ffb80ecb96be55882c94111e6e3
f.cinelli@bizcode.it <f.cinelli@bizcode.it>
2022-12-19 08:53:41 +0100

    Add start center button

--- marsrover.js
+++ marsrover.js
@@ -1,109 +1,127 @@
-
-
 class Planet {
-    constructor(squaredGridDimension = 10) {
-        this.maxX = squaredGridDimension - 1;
-        this.maxY = squaredGridDimension -1 ;
-    }
-
+	constructor(squaredGridDimension = 10) {
+		this.maxX = squaredGridDimension - 1;
+		this.maxY = squaredGridDimension - 1;
+	}
 }
 
-
 class Rover {
-    constructor(x = 0, y = 0, direction = "N") {
-        this.x = x;
-        this.y = y;
-        this.direction = direction;  //N S W E
-    }
+	constructor(x = 0, y = 0, direction = "N") {       
+		this.x = x;
+		this.y = y;
+		this.direction = direction; //N S W E
+	}
 
-    move(command){
-        console.log(`moving ${command}`)
-        if (command == "f"){
-            this.y = this.y + 1;
+	move(forward, roverX, roverY, roverDir) {
+        let roverSelectedX = roverX.x;
+        let roverSelectedY = roverY.y;
+        let roverSelectedDir = roverDir.direction;
+		console.log(`moving ${roverSelectedX}`);
+        console.log(`moving ${roverSelectedY}`);
+        console.log(`moving ${roverSelectedDir}`);
+
+        //planet limit
+        if ((roverSelectedDir == "N" && roverSelectedY <= 0) || (roverSelectedDir == "S" && roverSelectedY >= 9) || (roverSelectedDir == "E" && roverSelectedX >= 9) || 
+            (roverSelectedDir == "W" && roverSelectedX <=0)) {
+                alert("You cannot move in that direction, the rover has to stay on Mars");
+                
+        }
+        else if (roverSelectedDir == "N" && roverSelectedY <=9){
+            roverSelectedY = roverSelectedY - [forward];
+
+        }
+        else if (roverSelectedDir == "S" && roverSelectedY < 9){
+            roverSelectedY = roverSelectedY + [forward];
+
+        }
+        else if (roverSelectedDir == "E" && roverSelectedX < 9){
+            roverSelectedX = roverSelectedX + [forward];
+        }
+        else if (roverSelectedDir == "W" && roverSelectedX < 9){
+            roverSelectedX = roverSelectedX - [forward];
+        }
+       this.x = roverSelectedX;
+       this.y = roverSelectedY;
+
+
+	}
+
+	turn = (turnDirection) => {
+        let turn = turnDirection.toUpperCase();
+        if (turn == "N"){
+            this.direction = turn;
+        }
+        if (turn == "E"){
+            this.direction = turn;
+        }
+        if (turn == "W"){
+            this.direction = turn;
+        }
+        if (turn == "S"){
+            this.direction = turn;
         }
 
-        if (command == "b"){
-            this.y = this.y - 1;
-        }
     }
-
-    turn() {
-
-    }
-
 }
 
 class Obstacle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
 }
 
-const startMove = (rover) => {
-    const whereToGo = prompt("Plese let me know the direction, mars rover need to know it (ex : N or E or W or S):");
-    // const move = prompt("Choose if you want to move forward or move back ");
-     const direction = whereToGo.toUpperCase();
-     switch (direction) {
-         case 'N':
-             console.log(`you choose the ${direction} direction`);
-             rover.direction = direction;
-             break;
-             case 'E':
-                 console.log(`you choose the ${direction} direction`);
-                 rover.direction = direction;
-             break;
-                 case 'W':
-                     console.log(`you choose the ${direction} direction`);
-                     rover.direction = direction;
-             break;
-                     case 'S':
-                         console.log(`you choose the ${direction} direction`);
-                         rover.direction = direction;
-             break;
-     }
-     
-     whereIsMyRover(rover);
-     chooseMove(rover);
-}
+const startMove = (rover,mars) => {
+	let whereToGo = prompt(
+		"Plese let me know the direction, mars rover need to know it (ex : N or E or W or S):"
+	);
+    rover.turn(whereToGo);
+	whereIsMyRover(rover);
+	chooseMove(rover,mars);
+};
 
-const chooseMove = (roverSelected ) => {
-     const move = prompt('Choose if you want to move forward or move back (type "F" move forward / type "B"  move back)');
-      const move2 = move.toUpperCase();
-      switch (move2) {
-          case 'F':
-              console.log(`you choose to move ${move2}`);
-              roverSelected.y = roverSelected.y +1;
-              break;
-              case 'B':
-              console.log(`you choose to move ${move2}`);
-              roverSelected.y = roverSelected.y +1;
-              break;
-              default:
-                alert(`Use the right command, insert F or B`);
-      }
-      whereIsMyRover(roverSelected);
- }
+const chooseMove = (roverSelected) => {
+    let forward = prompt(
+		'Choose how far you want to go (choose a number)'
+	);
+    console.log("valore di forward",forward);
+	// let move2 = move.toUpperCase();
+    roverPos = roverSelected;
+	roverSelected.move(forward,roverPos.x,roverSelected.y,roverSelected.direction);
+};
 
-const checkPosition = () =>{
+const checkObstacle = (roverPosition) => {};
 
+const checkPlanetDimension = (roverX,roverY,roverDir) => {
 
-}
+};
 
-
-
-const whereIsMyRover  = (rover) => {
-    alert(`Now the Rover is X:${rover.x}, Y: ${rover.y}, facing: ${rover.direction}`);
-}
+const whereIsMyRover = (rover) => {
+    document.getElementById('roverposition').textContent = [`Now the Rover is X:${rover.x}, Y: ${rover.y}, facing: ${rover.direction}`];
+ 
+	alert(
+		`Now the Rover is X:${rover.x}, Y: ${rover.y}, facing: ${rover.direction}`
+	);
+};
 
 const start = () => {
     const rover = new Rover();
-    const mars = new Planet;
-    const obstacle = new Obstacle;
-    alert(`Welcome to Mars`)
-    startMove(rover);
+	const mars = new Planet();
+	const obstacle = new Obstacle();
+   
+	alert(`Welcome to Mars`);
+	alert(`Now your Mars Rover is in position X:0 , Y:0 ; Direction : `);
 
-}
+	startMove(rover);
+   
+};
 
+window.load = () => {
+	const c = document.getElementById("marsCanvas");
+	const img = document.getElementById("mars");
+	const ctx = c.getContext("2d");
+	ctx.drawImage(img, 1, 1);
+};
 
-start();
+window.load();
+
